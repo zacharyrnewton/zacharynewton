33514b228687d5a0b408a886aaba19b9659558f5
Zachary Newton <zacharyrnewton@gmail.com>
2018-10-27 13:23:12 -0400

    fixed CTA

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,13 @@
+//CTA Toggle
 $('.mainCTAWrapper, .closeCTA').on('click', function(){
   $('html').toggleClass('CTAopen');
+});
+
+//Typed JS
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 100,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true,
 });
