2b77e8710bff94ed9fd87027877713fcf760aac4
Zachary Newton <zacharyrnewton@gmail.com>
2018-10-28 14:36:45 -0400

    added 404 page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,12 +2,3 @@
 $('.mainCTAWrapper, .closeCTA').on('click', function(){
   $('html').toggleClass('CTAopen');
 });
-
-//Typed JS
-var typed = new Typed('#typed', {
-  stringsElement: '#typed-strings',
-  typeSpeed: 100,
-  backSpeed: 20,
-  backDelay: 1000,
-  loop: true,
-});
