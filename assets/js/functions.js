88fde4d40df603f6c140ec104bb4a4d65a1d1cc4
Zachary Newton <zacharyrnewton@gmail.com>
2019-04-23 09:41:00 -0400

    working on the navigation toggle for scrolled

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,13 @@
+//Nav Scrolled Style Switch
+$(window).on("load resize scroll",function(e){
+  var y = window.scrollY
+  if (y < 1) {
+    $('nav').removeClass('scrolled');
+  } else {
+    $('nav').addClass('scrolled');
+  }
+});
+
 //CTA Toggle
 $('.mainCTAWrapper, .closeCTA, .contact').on('click', function(){
   $('html').toggleClass('CTAopen');
